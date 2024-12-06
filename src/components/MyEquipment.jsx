82874062300d3eebc455c726bffa15ff82e1ac2d
Syed Muhimin Anam <76765021+syed-muhimin-anam@users.noreply.github.com>
2024-12-06 18:21:42 +0600

    impliment equipment delete features

--- src/components/MyEquipment.jsx
+++ src/components/MyEquipment.jsx
@@ -1,23 +1,54 @@
 import { useContext } from "react";
-import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProviders";
+import { Link, useLoaderData } from "react-router-dom";
 
 
 const MyEquipment = () => {
     const myEquipments = useLoaderData();
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const filteredEquipments = myEquipments.filter(myEquipment => myEquipment.userEmail === user?.email);
+    
+    const handleDelete = (_id) => {
+        fetch(`http://localhost:5000/equipments/${_id}`, {
+            method: 'DELETE',
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            
+            window.location.href = '/';
+        })
+       
+    };
+    
     return (
         <div>
             <h1>My equipment</h1>
             <h2>{filteredEquipments.length}</h2>
             {
-                filteredEquipments.map(equipment => <div key={equipment._id} className="equipment-card">
-                    <h2>{equipment.item}</h2>
+                filteredEquipments.map(equipment => <div key={equipment._id} className="">
+                    <div className="card card-compact bg-base-100 w-96 shadow-xl">
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{equipment.item}</h2>
+                            <p>{equipment.price}</p>
+                            <p> {equipment.category}</p>
+                            <h2>{equipment.item}</h2>
                     <p>Category: {equipment.category}</p>
                     <p>Price: ${equipment.price}</p>
                     <p>Stock: {equipment.stock}</p>
-                </div>  )
+                            <Link className="btn" to={`/update/${equipment._id}`}>
+                        <button>Update</button>
+                    </Link>
+                    <button className="btn" onClick={() => handleDelete(equipment._id)}>Delete</button>
+                        </div>
+                    </div>
+
+                </div>)
             }
         </div>
     );
--- src/components/ViewDetails.jsx
+++ src/components/ViewDetails.jsx
@@ -1,17 +1,16 @@
-import { Link, useLoaderData } from "react-router-dom";
 
 
 const ViewDetails = () => {
-    const equipmentsDetails = useLoaderData()
+    const equipmentsDetails = useLoaderData();
+
+
     return (
         <div>
-            <h1>{equipmentsDetails.category}</h1>
-            <h1>{equipmentsDetails.item}</h1>
-            <h1>{equipmentsDetails.price}</h1>
-            <h1>{equipmentsDetails.description}</h1>
-            <Link className="btn" to={`/update/${equipmentsDetails._id}`}><button>update</button></Link>
-            <Link className="btn"><button>delete</button></Link>
-            
+            <h1>Category: {equipmentsDetails.category}</h1>
+            <h1>Item: {equipmentsDetails.item}</h1>
+            <h1>Price: {equipmentsDetails.price}</h1>
+            <h1>Description: {equipmentsDetails.description}</h1>
+       
         </div>
     );
 };
