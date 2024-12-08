18e3efca8d2f2c739b4291769ee7b982be4b7f7e
Syed Muhimin Anam <76765021+syed-muhimin-anam@users.noreply.github.com>
2024-12-08 23:55:42 +0600

    done 3

--- src/components/MyEquipment.jsx
+++ src/components/MyEquipment.jsx
@@ -59,7 +59,7 @@ const MyEquipment = () => {
                         <div className="card card-compact bg-base-100 shadow-xl">
                             <figure>
                                 <img
-                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                    src={equipment.photo}
                                     alt="Equipment"
                                 />
                             </figure>
--- src/components/ViewDetails.jsx
+++ src/components/ViewDetails.jsx
@@ -5,7 +5,7 @@ const ViewDetails = () => {
 
     return (
         <div>
-            <h1 className="text-2xl font-bold my-5 text-center">Details</h1>
+        <h1 className="text-2xl font-bold my-5 text-center">Details</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center bg-base-100 shadow-xl">
 
                 <figure className="w-full h-64 overflow-hidden flex items-center justify-center">
